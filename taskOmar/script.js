b98d797ec3792b3896b5ae94d3f04d14c49ac107
devomarfaruk <112255596+devomarfaruk@users.noreply.github.com>
2022-12-04 21:11:04 +0600

    Add files via upload

---
+++ taskOmar/script.js
@@ -0,0 +1,27 @@
+//Task Omar------------------>>
+
+// for (i = 1; i <= 100; i++) {
+//     if (i % 15 == 0) {
+//         document.write("FizzBuzz" + " ");
+//     }
+
+//     else if (i % 3 == 0) {
+//         document.write("Fizz" + " ");
+//     }
+
+//     else if (i % 5 == 0) {
+//         document.write("Buzz" + " ");
+//     }
+//     else {
+//         document.write(i + " ");
+//     }
+
+// };
+
+
+//Question 1 Answer:
+// Variables.
+// Control Structures.
+// Data Structures.
+// Syntax.
+// Tools.
